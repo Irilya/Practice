4c24c8bbf156603201e071519e9c983bebca3be5
Irilya <irilya@mail.ru>
2021-11-24 11:14:49 +0300

    practice 16.7.1

---
+++ Cat/Cat2.py
@@ -0,0 +1,6 @@
+from Cats import Cat
+
+cat_1 = Cat('Барон', 'мальчик', '2 года')
+cat_2 = Cat('Сэм', "мальчик", '2 года')
+print('Первый кот - ', cat_1.name, cat_1.gen, cat_1.age)
+print('Второй кот - ', cat_2.name, cat_2.gen, cat_2.age)
---
+++ Cat/Cats.py
@@ -0,0 +1,14 @@
+class Cat:
+    def __init__(self,name, gen, age):
+        self.name = name
+        self.gen = gen
+        self.age = age
+
+    def getName(self):
+        return self.name
+
+    def getGen(self):
+        return self.gen
+
+    def getAge(self):
+        return self.age
