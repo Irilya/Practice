67b2ca1e6a58936948332f0b40ff4831e231804f
Irilya <irilya@mail.ru>
2021-12-21 11:52:02 +0300

    update

--- CryptoBot/extensions.py
+++ CryptoBot/extensions.py
@@ -27,9 +27,8 @@ class ValuteConverter:
             raise ConvertionException(f'Неверно указано количество валюты {amount}')
 
 
-        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
-        total_base = json.loads(r.content)[base_ticker]
+        r = requests.get(f'https://free.currconv.com/api/v7/convert?q={quote_ticker}_{base_ticker}&compact=ultra&apiKey=a7f29e076bb4816202bd')
+        total_base = json.loads(r.content)[f'{quote_ticker}_{base_ticker}']
         total_valut = float(total_base) * float(amount)
-
         return total_valut
 
